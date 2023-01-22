bdd1c47fa138129754ddabd785252e41aa194625
Kyuorhan <jkprogrammer96@gmail.com>
2023-01-22 09:27:29 -0400

    Added new depenciess of back-end

---
+++ server/src/server.ts
@@ -0,0 +1,29 @@
+import Fastify from 'fastify';
+import Cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(Cors)
+
+/* Metodo HTTP: Get, Post, Put, Patch, Delete */
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Exerc'
+            }
+        }
+
+    })
+
+    return habits
+})
+
+app.listen({
+    port: 3333
+}).then(() => {
+    console.log('HTTP Server running!')
+})
