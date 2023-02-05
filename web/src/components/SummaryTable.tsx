70e120ff1025aa9dae97a29b2093d0139801cd82
Kyuorhan <jkprogrammer96@gmail.com>
2023-02-04 23:05:57 -0400

    Added news components (Modal, Popover) in environment web

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -18,21 +18,30 @@ const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
   return (    
-    <div className="
-      w-full
-      flex"
+    <div 
+      className="
+        w-full
+        flex"
     >
-      <div className="
-        grid 
-        grid-rows-7
-        grid-flow-row
-        gap-3"
+      <div 
+        className="
+          grid 
+          grid-rows-7
+          grid-flow-row
+          gap-3"
       >
         {weekDays.map((weekDay, i) => {
           return (
             <div 
               key= {`${weekDay}-${i}`} 
-              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+              className="
+                text-zinc-400 
+                text-xl 
+                h-10 w-10 
+                font-bold 
+                flex 
+                items-center 
+                justify-center"
             >
               {weekDay}
             </div>            
@@ -40,14 +49,21 @@ export function SummaryTable() {
         })}
       </div>     
 
-      <div className="
-        grid 
-        grid-rows-7
-        grid-flow-col
-        gap-3"
+      <div 
+        className="
+          grid 
+          grid-rows-7
+          grid-flow-col
+          gap-3"
       >
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return (
+            <HabitDay 
+              key={date.toString()} 
+              amount={5} 
+              completed={Math.round(Math.random() * 5)} 
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
